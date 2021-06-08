f6a8246ff28acc943e65326b609b5fcf282d512f
kostdani <kostdani@fit.cvut.cz>
2021-06-08 09:01:46 +0200

    CLion files

---
+++ semestral/src/cmake-build-debug/CMakeFiles/Server.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kostdani/Documents/CVUT/PA2/Server/Accepter.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Accepter.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Actor.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Actor.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Client.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Client.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Counter.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Counter.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Epoller.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Epoller.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Message.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Message.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Queuer.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Queuer.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Reciever.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Reciever.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/Timer.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/Timer.cpp.o"
+  "/home/kostdani/Documents/CVUT/PA2/Server/main.cpp" "/home/kostdani/Documents/CVUT/PA2/Server/cmake-build-debug/CMakeFiles/Server.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ semestral/src/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/Accepter.cpp.o"
+  "CMakeFiles/Server.dir/Actor.cpp.o"
+  "CMakeFiles/Server.dir/Client.cpp.o"
+  "CMakeFiles/Server.dir/Counter.cpp.o"
+  "CMakeFiles/Server.dir/Epoller.cpp.o"
+  "CMakeFiles/Server.dir/Message.cpp.o"
+  "CMakeFiles/Server.dir/Queuer.cpp.o"
+  "CMakeFiles/Server.dir/Reciever.cpp.o"
+  "CMakeFiles/Server.dir/Timer.cpp.o"
+  "CMakeFiles/Server.dir/main.cpp.o"
+  "Server"
+  "Server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
