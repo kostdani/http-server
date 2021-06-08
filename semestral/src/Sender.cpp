76362762adcfed8514556d9048105b341a48c765
kostdani <kostdani@fit.cvut.cz>
2021-06-08 10:46:38 +0200

    Sender class

--- semestral/src/Sender.cpp
+++ semestral/src/Sender.cpp
@@ -4,3 +4,15 @@
 
 #include "Sender.h"
 
+Sender::Sender(int descriptor) :Queuer(){
+    m_socketfd=descriptor;
+}
+
+bool Sender::multiplex(int epolld) {
+    Queuer::multiplex(epolld);
+    epoll_event ev{};
+    ev.data.ptr = this;
+    ev.events = EPOLLOUT | EPOLLET;
+    return epoll_ctl(epolld, EPOLL_CTL_ADD, m_socketfd, &ev) == 0;
+
+}
--- semestral/src/Sender.h
+++ semestral/src/Sender.h
@@ -4,11 +4,37 @@
 
 #ifndef SERVER_SENDER_H
 #define SERVER_SENDER_H
-#include "Actor.h"
+#include "Queuer.h"
 
-class Sender {
+class Sender: public Queuer {
+public:
+    Sender(int descriptor);
 
+
+    void onInput() override;
+
+    void onOutput() override;
+
+    void onError() override;
+
+protected:
+    int m_socketfd=-1;
+
+    bool multiplex(int epolld) override;
 };
+
+
+void Sender::onInput() {
+    printf("sender input %d\n",m_descriptor);
+}
+void Sender::onOutput() {
+    printf("sender out %d\n",m_descriptor);
+}
+
+void Sender::onError() {
+    printf("sender error\n");
+}
+
 
 
 #endif //SERVER_SENDER_H
