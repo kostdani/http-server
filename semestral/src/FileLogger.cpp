b55feb785271b45f4910c9f1429af6d2b4893bed
kostdani <kostdani@fit.cvut.cz>
2021-06-15 17:35:44 +0200

    fixes

---
+++ semestral/src/FileLogger.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kostdani on 6/15/21.
+//
+
+#include "FileLogger.h"
---
+++ semestral/src/FileLogger.h
@@ -0,0 +1,14 @@
+//
+// Created by kostdani on 6/15/21.
+//
+
+#ifndef SRC_FILELOGGER_H
+#define SRC_FILELOGGER_H
+
+
+class FileLogger {
+
+};
+
+
+#endif //SRC_FILELOGGER_H
