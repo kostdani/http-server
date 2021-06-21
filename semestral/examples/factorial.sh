10afb441f4458406d7791c09a7963a80f1da7793
kostdani <kostdani@fit.cvut.cz>
2021-06-21 20:29:51 +0500

    Scripts and makefile fixes

--- semestral/examples/factorial.sh
+++ semestral/examples/factorial.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
-
 num=$1
 fact=1
 
-while [ $num -gt 1 ]
-do
-  fact=$((fact * num))  #fact = fact * num
-  num=$((num - 1))      #num = num - 1
-done
 
 echo "<!DOCTYPE html>
 <html>
 <body>
-<h1>Simple factorial script</h1>
-<p>Factorial of $1 is $fact.</p>
+<h1>Simple factorial script</h1><p>"
+
+if [[ "$#" -ne 1 ]]; then
+  echo "Usage: 127.0.0.1:8080/factorial?num"
+else
+while [ $num -gt 1 ]
+  do
+    fact=$((fact * num))  #fact = fact * num
+    num=$((num - 1))      #num = num - 1
+  done
+  echo "Factorial of $1 is $fact."
+fi
+
+echo "</p>
 </body>
 </html>"
--- semestral/examples/sum.sh
+++ semestral/examples/sum.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-for arg in "$@"
-do
-index=$(echo $arg | cut -f1 -d=)
-val=$(echo $arg | cut -f2 -d=)
-case $index in
-X) x=$val;;
-
-Y) y=$val;;
-
-*)
-esac
-done
-((result=x+y))
-
 echo "<!DOCTYPE html>
 <html>
 <body>
 <h1>Simple sum script</h1>
-<p>X+Y=$result.</p>
+<p>"
+
+if [[ "$#" -ne 2 ]]; then
+  echo "Usage: 127.0.0.1:8080/sum?X=num&Y=num"
+else
+  for arg in "$@"
+  do
+    index=$(echo $arg | cut -f1 -d=)
+    val=$(echo $arg | cut -f2 -d=)
+    case $index in
+      X) x=$val;;
+      Y) y=$val;;
+      *)
+    esac
+  done
+  ((result=x+y))
+  echo "X+Y=$x+$y=$result."
+fi
+
+echo "</p>
 </body>
 </html>"
